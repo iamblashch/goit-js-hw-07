00e6e01beb29322eadb47b8e8dbb52769a68b7bc
iamblashch <bblashchuk08@gmail.com>
2022-12-08 21:05:05 +0200

    js-7-hom-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector(".gallery");
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerRef.insertAdjacentHTML("beforeend",imagesMarkup);
+
+function createItemsMarkup(items){
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original.value}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+const onContainerClick = (evt) => {
+    evt.preventDefault();
+  
+    if (evt.target.classList.contains("gallery")) 
+     return;
+      const source = evt.target.dataset.source;
+      
+    const instance = basicLightbox.create(`
+      <img src="${source}"width="800" height="600">`);
+  
+    instance.show();
+  };
+  
+galleryContainerRef.addEventListener("click", onContainerClick);
